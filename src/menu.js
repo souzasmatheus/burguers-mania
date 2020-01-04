492b5c7ccae2f18968b772c2473dd970954c08de
Matheus Souza <matheu.sk.9@hotmail.com>
2020-01-04 20:41:49 -0300

    Write menu file

---
+++ src/menu.js
@@ -0,0 +1,209 @@
+export default {
+  Lanches: {
+    imgPath: 'https://image.flaticon.com/icons/svg/1409/1409167.svg',
+    list: {
+      'Nº 1': {
+        imgPath: 'https://image.flaticon.com/icons/svg/1522/1522452.svg',
+        info: 'Pão com gergelim, 2 carnes, 2 queijos, salada e molho especial',
+        price: '8,00'
+      },
+      'Nº 2': {
+        imgPath: 'https://image.flaticon.com/icons/svg/1522/1522452.svg',
+        info: 'Pão careca, 2 carnes, 2 queijos, salada e molho acebolado',
+        price: '7,50'
+      },
+      'Nº 3': {
+        imgPath: 'https://image.flaticon.com/icons/svg/1522/1522452.svg',
+        info:
+          'Pão com gergelim, 1 carne de picanha, 1 queijo, salada e molho especial',
+        price: '9,00'
+      },
+      'Nº 4': {
+        imgPath: 'https://image.flaticon.com/icons/svg/1522/1522452.svg',
+        info:
+          'Pão careca, 2 carnes, 2 queijos, picles, salada e molho especial',
+        price: '7,50'
+      },
+      'Nº 5': {
+        imgPath: 'https://image.flaticon.com/icons/svg/1522/1522452.svg',
+        info:
+          'Pão com gergelim, 2 carnes de picanha, 2 queijos, salada e molho especial',
+        price: '10,50'
+      },
+      'Nº 6': {
+        imgPath: 'https://image.flaticon.com/icons/svg/1522/1522452.svg',
+        info:
+          'Pão com gergelim, 2 carnes, 3 queijos, bacon, salada e molho especial',
+        price: '8,50'
+      },
+      'Nº 7': {
+        imgPath: 'https://image.flaticon.com/icons/svg/1522/1522452.svg',
+        info: 'Pão careca, 3 carnes, 3 queijos, salada e molho especial',
+        price: '7,50'
+      },
+      'Nº 8': {
+        imgPath: 'https://image.flaticon.com/icons/svg/1522/1522452.svg',
+        info: 'Pão careca, 1 carne, queijo, salada e molho especial',
+        price: '5,00'
+      },
+      'Nº 9': {
+        imgPath: 'https://image.flaticon.com/icons/svg/1522/1522452.svg',
+        info: 'Pão com gergelim, 4 carnes, 4 queijos, salada e molho especial',
+        price: '10,00'
+      },
+      'Nº 10': {
+        imgPath: 'https://image.flaticon.com/icons/svg/1522/1522452.svg',
+        info:
+          'Pão com gergelim, 2 carnes, 3 queijos, calabresa, 1 ovo, cheddar, bacon, salada e molho especial',
+        price: '10,50'
+      },
+      'Nº 11': {
+        imgPath: 'https://image.flaticon.com/icons/svg/1522/1522452.svg',
+        info:
+          'Pão com gergelim, 4 carnes, 5 queijos, calabresa, bacon, cheddar, salada e molho especial',
+        price: '12,90'
+      },
+      'Nº 12': {
+        imgPath: 'https://image.flaticon.com/icons/svg/1522/1522452.svg',
+        info:
+          'Pão com gergelim, 4 carnes, 5 queijos, 3 ovos, 2 bacons, salada e molho especial',
+        price: '15,00'
+      },
+      'Nº 13': {
+        imgPath: 'https://image.flaticon.com/icons/svg/1522/1522452.svg',
+        info:
+          'Pão com gergelim, filé de frango, queijo, catupiry, salada e molho especial',
+        price: '9,50'
+      },
+      'Nº 14': {
+        imgPath: 'https://image.flaticon.com/icons/svg/1522/1522452.svg',
+        info:
+          'Pão com gergelim, contra filé, queijo, cebola roxa, salada e molho barbecue',
+        price: '14,50'
+      },
+      'Nº 15': {
+        imgPath: 'https://image.flaticon.com/icons/svg/1522/1522452.svg',
+        info:
+          'Pão com gergelim, 6 carnes, 6 queijos, 3 ovos, filé de frango, 2 bacons, calabresa, cheddar, salada e molho especial',
+        price: '19,00'
+      },
+      'Nº 16': {
+        imgPath: 'https://image.flaticon.com/icons/svg/1522/1522452.svg',
+        info:
+          'Pão com gergelim, 2 linguiças toscanas, queijo, cebola roxa, ovo, salada e molho especial',
+        price: '13,50'
+      },
+      'Nº 17': {
+        imgPath: 'https://image.flaticon.com/icons/svg/1522/1522452.svg',
+        info:
+          'Pão careca, 1 carne de picanha, queijo padrão, banana, bacon e molho especial',
+        price: '12,00'
+      },
+      'Nº 18': {
+        imgPath: 'https://image.flaticon.com/icons/svg/1522/1522452.svg',
+        info:
+          'Pão com gergelim, cream cheese, peito de peru, orégano, queijo, filé de frango, bacon, cebola roxa, salada e molho especial',
+        price: '13,00'
+      }
+    }
+  },
+  Ingredientes: {
+    imgPath: 'https://image.flaticon.com/icons/svg/1046/1046774.svg',
+    list: {
+      Carne: {
+        imgPath: 'https://image.flaticon.com/icons/svg/123/123281.svg',
+        info:
+          'Nº 1, Nº 2, Nº 4, Nº 6, Nº 7, Nº 8, Nº 9, Nº 10, Nº 11, Nº 12 e Nº 15'
+      },
+      'Carne de Picanha': {
+        imgPath: 'https://image.flaticon.com/icons/svg/123/123281.svg',
+        info: 'Nº 3 e Nº 17'
+      },
+      'Contra Filé': {
+        imgPath: 'https://image.flaticon.com/icons/svg/123/123281.svg',
+        info: 'Nº 14'
+      },
+      Ovo: {
+        imgPath: 'https://image.flaticon.com/icons/svg/1046/1046774.svg',
+        info: 'Nº10, Nº12, Nº15 e Nº16'
+      },
+      'Molho Especial': {
+        imgPath: 'https://image.flaticon.com/icons/svg/1973/1973383.svg',
+        info:
+          'Nº1, Nº3, Nº4, Nº5, Nº6, Nº7, Nº8, Nº9, Nº10, Nº11, Nº12, Nº13, Nº15, Nº16, Nº17 e Nº18'
+      },
+      'Molho Acebolado': {
+        imgPath: 'https://image.flaticon.com/icons/svg/1973/1973383.svg',
+        info: 'Nº 2'
+      },
+      'Molho Barbecue': {
+        imgPath: 'https://image.flaticon.com/icons/svg/1973/1973383.svg',
+        info: 'Nº 14'
+      },
+      Queijo: {
+        imgPath: 'https://image.flaticon.com/icons/svg/2151/2151941.svg',
+        info:
+          'Nº1, Nº2, Nº3, Nº4, Nº5, Nº6, Nº7, Nº8, Nº9, Nº10, Nº11, Nº12, Nº13, Nº14, Nº15, Nº16, Nº17 e Nº18'
+      },
+      Cheddar: {
+        imgPath: 'https://image.flaticon.com/icons/svg/2151/2151941.svg',
+        info: 'Nº10, Nº11 e Nº15'
+      },
+      Catupiry: {
+        imgPath: 'https://image.flaticon.com/icons/svg/2151/2151941.svg',
+        info: 'Nº 13'
+      },
+      'Queijo Padrão': {
+        imgPath: 'https://image.flaticon.com/icons/svg/2151/2151941.svg',
+        info: 'Nº 17'
+      },
+      'Cebola Roxa': {
+        imgPath: 'https://image.flaticon.com/icons/svg/517/517610.svg',
+        info: 'Nº 14, Nº16 e Nº18'
+      },
+      'Pão com Gergelim': {
+        imgPath: 'https://image.flaticon.com/icons/svg/347/347606.svg',
+        info:
+          'Nº 1, Nº 3, Nº 5, Nº 6, Nº 9, Nº 10, Nº11, Nº 12, Nº 13, Nº 14, Nº 15, Nº 16 e Nº18'
+      },
+      'Pão Careca': {
+        imgPath: 'https://image.flaticon.com/icons/svg/347/347606.svg',
+        info: 'Nº 2, Nº 4, Nº 7, Nº 8 e Nº 17'
+      },
+      Bacon: {
+        imgPath: 'https://image.flaticon.com/icons/svg/1702/1702824.svg',
+        info: 'Nº 6, Nº 10, Nº 11, Nº 12, Nº 15, Nº 17 e Nº 18'
+      },
+      Picles: {
+        imgPath:
+          'https://www.flaticon.com/premium-icon/icons/svg/2079/2079331.svg',
+        info: 'Nº 4'
+      },
+      Calabresa: {
+        imgPath: 'https://image.flaticon.com/icons/svg/1070/1070815.svg',
+        info: 'Nº 10, Nº11 e Nº15'
+      },
+      'Linguiça Toscana': {
+        imgPath: 'https://image.flaticon.com/icons/svg/1070/1070815.svg',
+        info: 'Nº 16'
+      },
+      'Filé de Frango': {
+        imgPath: 'https://image.flaticon.com/icons/svg/1046/1046751.svg',
+        info: 'Nº 13, Nº 15 e Nº18'
+      },
+      Banana: {
+        imgPath: 'https://image.flaticon.com/icons/svg/1514/1514933.svg',
+        info: 'Nº 17'
+      },
+      'Peito de Peru': {
+        imgPath:
+          'https://www.flaticon.com/premium-icon/icons/svg/2403/2403459.svg',
+        info: 'Nº18'
+      },
+      Orégano: {
+        imgPath: 'https://image.flaticon.com/icons/svg/1398/1398262.svg',
+        info: 'Nº18'
+      }
+    }
+  }
+};
