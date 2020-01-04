94b1b86218485fdbd38e3a3c92c6bd9cf77f8ee7
Matheus Souza <matheu.sk.9@hotmail.com>
2020-01-03 22:14:59 -0300

    Render types-container section and style application

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Card from './components/Card/Card';
 
-function App() {
-  return <div className="app-container" />;
+function App({ menu }) {
+  const [selectedType, setSelectedType] = useState(true);
+
+  const types = [];
+  for (let key in menu) {
+    types.push(
+      <Card
+        imgPath={menu[key].imgPath}
+        text={`${key}`}
+        isSelected={selectedType === key ? true : false}
+        handler={setSelectedType}
+      />
+    );
+  }
+
+  return (
+    <div className="app-container">
+      <div
+        className="types-container"
+        style={
+          selectedType
+            ? { paddingBottom: '5%', borderBottom: 'solid 1px #bbb' }
+            : {}
+        }
+      >
+        {types}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import menu from './menu';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App menu={menu} />, document.getElementById('root'));
