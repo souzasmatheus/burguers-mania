734deea3d7c16c9298f155ebc80e6411d87e13dc
Matheus Souza <matheu.sk.9@hotmail.com>
2020-01-04 21:08:59 -0300

    Updates

---
+++ precache-manifest.cb1f9f313a449050ade02cc98d4592fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03f85e4881dd18dc3090c283240944ec",
+    "url": "/burguers-mania/index.html"
+  },
+  {
+    "revision": "11461a69b7c01d6e9609",
+    "url": "/burguers-mania/static/css/main.dae9b6fa.chunk.css"
+  },
+  {
+    "revision": "35f5854c79567ddbe454",
+    "url": "/burguers-mania/static/js/2.539f2b53.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/burguers-mania/static/js/2.539f2b53.chunk.js.LICENSE"
+  },
+  {
+    "revision": "11461a69b7c01d6e9609",
+    "url": "/burguers-mania/static/js/main.12885c8e.chunk.js"
+  },
+  {
+    "revision": "2267dc6629ee26f7cc43",
+    "url": "/burguers-mania/static/js/runtime-main.c95cef6f.js"
+  }
+]);
